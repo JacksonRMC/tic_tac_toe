e5e6d512036a127eef9b40b17262efb15dbd57fa
Jackson Carter <jcarter0803@gmail.com>
2017-06-19 15:39:38 -0700

    not sure where to go

--- index.js
+++ index.js
@@ -3,18 +3,12 @@ const prompt = require('prompt');
 
 prompt.start();
 
- prompt.get(['username', 'email'], function (err, result) {
-    //
-    // Log the results.
-    //
-    console.log('Command-line input received:');
-    console.log('  username: ' + result.username);
-    console.log('  email: ' + result.email);
-  });
-// const ticTacToe = () => {
-//   let user1 = prompt('First move?')
 
-//   let newBoard = new Array(['-','-','-'], ['-','-','-'], ['-','-','-'])
-//   return newBoard
-// }
+const ticTacToe = () => {
+  let user1 = prompt('First move?')
 
+  let newBoard = new Array(['-','-','-'], ['-','-','-'], ['-','-','-'])
+  return newBoard
+}
+
+console.log(ticTacToe());
