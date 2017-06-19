4b48cd509d1e55c8156fc5bf3128171c656fa2ca
Jackson Carter <jcarter0803@gmail.com>
2017-06-19 15:38:59 -0700

    Almost got it

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 #! /usr/bin/env node
+const prompt = require('prompt');
+
+prompt.start();
+
+ prompt.get(['username', 'email'], function (err, result) {
+    //
+    // Log the results.
+    //
+    console.log('Command-line input received:');
+    console.log('  username: ' + result.username);
+    console.log('  email: ' + result.email);
+  });
+// const ticTacToe = () => {
+//   let user1 = prompt('First move?')
+
+//   let newBoard = new Array(['-','-','-'], ['-','-','-'], ['-','-','-'])
+//   return newBoard
+// }
 
